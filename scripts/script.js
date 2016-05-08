527c040dc7c3a3b48c115fd4b9726fcfad173ab6
iamnatsu <natsuho.wada@gmail.com>
2016-05-08 21:57:41 +0900

    created flame

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $('#header').load('https://iamnatsu.github.io/view/header.html');
+  $('#footer').load('https://iamnatsu.github.io/view/footer.html');
+  $('#side_menu').load('https://iamnatsu.github.io/view/side_menu.html');
+});
